7468392f6b89067de6921714224f7f0d3b381f5f
Ben Thomas <github@bigredgiraffe.com>
2015-12-22 22:33:46 -0800

    added slack

--- lib/desktop/13-chat.sh
+++ lib/desktop/13-chat.sh
@@ -6,4 +6,5 @@ brew cask install --appdir="/Applications" telegram
 brew cask install --appdir="/Applications" adium
 brew cask install --appdir="/Applications" skype
 brew cask install --appdir="/Applications" hipchat
+brew cask install --appdir="/Applications" slack
 brew cask install --appdir="/Applications" google-hangouts
