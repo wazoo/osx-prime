512dfead9d97dc2a1761c4f93d75feb16a5df795
Ben Thomas <github@bigredgiraffe.com>
2015-12-23 09:29:11 -0800

    dotfiles was using current dir not /Users/wazoo

--- lib/cli_config/04-dotfiles.sh
+++ lib/cli_config/04-dotfiles.sh
@@ -7,50 +7,50 @@ fi
 
 # Global Env Vars
 if [ ! -h $HOME/.env_vars ]; then
-  ln -s $(pwd)/.osx-prime/dotfiles/env_vars $HOME/.env_vars
+  ln -s $HOME/.osx-prime/dotfiles/env_vars $HOME/.env_vars
 else
   echo "Found existing dotfile symlink for $HOME/.env_vars"
 fi
 
 # Global Aliases
 if [ ! -h $HOME/.aliases ]; then
-  ln -s $(pwd)/.osx-prime/dotfiles/aliases $HOME/.aliases
+  ln -s $HOME/.osx-prime/dotfiles/aliases $HOME/.aliases
 else
   echo "Found existing dotfile symlink for $HOME/.env_vars"
 fi
 # Global Functions
 if [ ! -h $HOME/.functions ]; then
-  ln -s $(pwd)/.osx-prime/dotfiles/functions $HOME/.functions
+  ln -s $HOME/.osx-prime/dotfiles/functions $HOME/.functions
 else
   echo "Found existing dotfile symlink for $HOME/.functions"
 fi
 # Global Editorconfig
 if [ ! -h $HOME/.editorconfig ]; then
-  ln -s $(pwd)/.osx-prime/dotfiles/editorconfig $HOME/.editorconfig
+  ln -s $HOME/.osx-prime/dotfiles/editorconfig $HOME/.editorconfig
 else
   echo "Found existing dotfile symlink for $HOME/.editorconfig"
 fi
 
 # Global Git Config
 if [ ! -h $HOME/.gitignore ]; then
-  ln -s $(pwd)/.osx-prime/dotfiles/gitignore $HOME/.gitignore
+  ln -s $HOME/.osx-prime/dotfiles/gitignore $HOME/.gitignore
 else
   echo "Found existing dotfile symlink for $HOME/.gitignore"
 fi
 if [ ! -h $HOME/.gitconfig ]; then
-  ln -s $(pwd)/.osx-prime/dotfiles/gitconfig $HOME/.gitconfig
+  ln -s $HOME/.osx-prime/dotfiles/gitconfig $HOME/.gitconfig
 else
   echo "Found existing dotfile symlink for $HOME/.gitconfig"
 fi
 
 # Multiplexers
 if [ ! -h $HOME/.screenrc ]; then
-  ln -s $(pwd)/.osx-prime/dotfiles/screenrc $HOME/.screenrc
+  ln -s $HOME/.osx-prime/dotfiles/screenrc $HOME/.screenrc
 else
   echo "Found existing dotfile symlink for $HOME/.screenrc"
 fi
 if [ ! -h $HOME/.tmux.conf ]; then
-  ln -s $(pwd)/.osx-prime/dotfiles/tmux.conf $HOME/.tmux.conf
+  ln -s $HOME/.osx-prime/dotfiles/tmux.conf $HOME/.tmux.conf
 else
   echo "Found existing dotfile symlink for $HOME/.tmux.conf"
 fi
