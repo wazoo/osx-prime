020d2343f3b125fe2bec6cd1d63e374fa1b429e5
Ben Thomas <github@bigredgiraffe.com>
2015-12-23 09:40:39 -0800

    fixed some prezto bugs

--- lib/cli_config/11-zsh.sh
+++ lib/cli_config/11-zsh.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 # Install Prezto : https://github.com/sorin-ionescu/prezto
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+if [[ ! -d "$HOME/.zprezto" ]]; then
+  git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+fi
+
 
 # Need to run the rest of this with zsh
 zsh lib/cli_config/12-prezto.zsh
--- lib/cli_config/12-prezto.zsh
+++ lib/cli_config/12-prezto.zsh
@@ -1,17 +1,17 @@
 #!/usr/bin/env zsh
 
-if [ ! -h $HOME/.zlogin]; then
+if [ ! -d "$HOME/.zprezto" ]; then
   setopt EXTENDED_GLOB
   for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
     ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
   done
 fi
 # Install .zshrc
-if [ ! -h $HOME/.zshrc]; then
+if [ ! -h "$HOME/.zshrc" ]; then
   rm -rf $HOME/.zshrc
   ln -s $HOME/.osx-prime/dotfiles/zshrc $HOME/.zshrc
 fi
-if [ ! -h $HOME/.zpreztorc ]; then
+if [ ! -h "$HOME/.zpreztorc" ]; then
   rm -rf $HOME/.zpreztorc
   ln -s $HOME/.osx-prime/dotfiles/zpreztorc $HOME/.zpreztorc
 fi
