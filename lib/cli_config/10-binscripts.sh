4160b77a945737f876f10238f875c8bb84e51dbb
Ben Thomas <github@bigredgiraffe.com>
2015-12-23 09:50:40 -0800

    changed binscripts to be symlinks

--- lib/cli_config/10-binscripts.sh
+++ lib/cli_config/10-binscripts.sh
@@ -5,7 +5,9 @@ if [ ! -d "$HOME/bin" ]; then
   mkdir $HOME/bin
 fi
 
-rm -rf $HOME/bin/*
-cp binscripts/* $HOME/bin/
-
-chmod +x $HOME/bin/*
+find "$HOME/.osx-prime/binscripts" -type f -exec basename {} \; |
+while read script
+do
+  echo "Linking: $script"
+  ln -s "$HOME/.osx-prime/binscripts/$script" "$HOME/bin/$script"
+done
