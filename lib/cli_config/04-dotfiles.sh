e7f5e62b2036a93ea083f27e68b5bc2a54608504
Ben Thomas <github@bigredgiraffe.com>
2015-12-23 09:53:17 -0800

    changed dotfiles to be a loop

--- lib/cli_config/04-dotfiles.sh
+++ lib/cli_config/04-dotfiles.sh
@@ -5,55 +5,16 @@ if [ ! -d "$HOME/.osx-prime" ]; then
   git clone https://github.com/wazoo/osx-prime $HOME/.osx-prime
 fi
 
-# Global Env Vars
-if [ ! -h $HOME/.env_vars ]; then
-  ln -s $HOME/.osx-prime/dotfiles/env_vars $HOME/.env_vars
-else
-  echo "Found existing dotfile symlink for $HOME/.env_vars"
-fi
-
-# Global Aliases
-if [ ! -h $HOME/.aliases ]; then
-  ln -s $HOME/.osx-prime/dotfiles/aliases $HOME/.aliases
-else
-  echo "Found existing dotfile symlink for $HOME/.env_vars"
-fi
-# Global Functions
-if [ ! -h $HOME/.functions ]; then
-  ln -s $HOME/.osx-prime/dotfiles/functions $HOME/.functions
-else
-  echo "Found existing dotfile symlink for $HOME/.functions"
-fi
-# Global Editorconfig
-if [ ! -h $HOME/.editorconfig ]; then
-  ln -s $HOME/.osx-prime/dotfiles/editorconfig $HOME/.editorconfig
-else
-  echo "Found existing dotfile symlink for $HOME/.editorconfig"
-fi
-
-# Global Git Config
-if [ ! -h $HOME/.gitignore ]; then
-  ln -s $HOME/.osx-prime/dotfiles/gitignore $HOME/.gitignore
-else
-  echo "Found existing dotfile symlink for $HOME/.gitignore"
-fi
-if [ ! -h $HOME/.gitconfig ]; then
-  ln -s $HOME/.osx-prime/dotfiles/gitconfig $HOME/.gitconfig
-else
-  echo "Found existing dotfile symlink for $HOME/.gitconfig"
-fi
-
-# Multiplexers
-if [ ! -h $HOME/.screenrc ]; then
-  ln -s $HOME/.osx-prime/dotfiles/screenrc $HOME/.screenrc
-else
-  echo "Found existing dotfile symlink for $HOME/.screenrc"
-fi
-if [ ! -h $HOME/.tmux.conf ]; then
-  ln -s $HOME/.osx-prime/dotfiles/tmux.conf $HOME/.tmux.conf
-else
-  echo "Found existing dotfile symlink for $HOME/.tmux.conf"
-fi
+find "$HOME/.osx-prime/dotfiles" -type f -exec basename {} \; |
+while read dotfile
+do
+  if [ ! -h "$HOME/$dotfile"]; then
+    echo "Linking Dotfile: $dotfile"
+    ln -s "$HOME/.osx-prime/dotfiles/$dotfile" "$HOME/$dotfile"
+  else
+    echo "Found existing dotfile link for $HOME/$dotfile"
+  fi
+done
 
 if [ ! -d "$HOME/.atom" ]; then
   mkdir $HOME/.atom/
